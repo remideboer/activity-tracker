891c708a32a3e1684dacdeddc2c92369608e37c7
Remi de Boer <remi@rsvier.nl>
2019-08-25 00:19:43 +0200

    refactored ActivityTrackingForegroundService to use single Intent and a method to create the pending intent. Getting rid of code duplication and too much fields.

--- app/src/main/java/com/remideboer/freeactive/services/ActivityTrackingForegroundService.kt
+++ app/src/main/java/com/remideboer/freeactive/services/ActivityTrackingForegroundService.kt
@@ -24,71 +24,47 @@ private const val ACTION_RESUME = "RESUME"
 class ActivityTrackingForegroundService : Service() {
 
     private val TAG = ActivityTrackingForegroundService::class.java.simpleName
-
-    private val notificationManager: NotificationManager by lazy {
+    private val notificationManager: NotificationManager by lazy(LazyThreadSafetyMode.NONE) {
         getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
     }
-
     private val notificationBuilder by lazy(LazyThreadSafetyMode.NONE) {
         NotificationCompat.Builder(this, App.NOTIFICATION_CHANNEL_ID)
-            .setContentTitle("Free Active is Tracking")
+            .setContentTitle(resources.getString(R.string.notification_tracker_title))
             .setSmallIcon(R.drawable.ic_icon_eye)
-            .setOngoing(true)
+            .setOngoing(true) // so it can't be dismissed by the user
             // PRIORITY_MAX shows expanded notificationBuilder to show action button (add setWhen(0) for older versions if needed)
             .setPriority(NotificationCompat.PRIORITY_MAX)
             // icons don't show using icons here, only needed for older version. Use custom layout for icons
             .addAction(
                 R.drawable.ic_icon_stop,
                 resources.getString(R.string.notification_stop),
-                stopPendingIntent
+                createPendingIntent(intent.apply { action = ACTION_STOP })
             )
             .addAction(
                 R.drawable.ic_icon_pause,
                 resources.getString(R.string.notification_pause),
-                pausePendingIntent
+                createPendingIntent(intent.apply { action = ACTION_PAUSE })
             )
             .addAction(
                 R.drawable.ic_icon_play,
                 resources.getString(R.string.notification_resume),
-                resumePendingIntent
+                createPendingIntent(intent.apply { action = ACTION_RESUME })
             )
     }
-    private val stopPendingIntent by lazy(LazyThreadSafetyMode.NONE) {
-        PendingIntent.getService(
-            this, 0,
-            stopIntent,
-            PendingIntent.FLAG_CANCEL_CURRENT
-        )
-    }
-    private val pausePendingIntent by lazy(LazyThreadSafetyMode.NONE) {
-        PendingIntent.getService(
-            this, 0,
-            pauseIntent,
-            PendingIntent.FLAG_CANCEL_CURRENT
-        )
-    }
-    private val resumePendingIntent by lazy(LazyThreadSafetyMode.NONE) {
-        PendingIntent.getService(
-            this, 0,
-            resumeIntent,
-            PendingIntent.FLAG_CANCEL_CURRENT
-        )
-    }
-    private val stopIntent by lazy(LazyThreadSafetyMode.NONE) {
+    private val intent by lazy(LazyThreadSafetyMode.NONE) {
         Intent(this, ActivityTrackingForegroundService::class.java)
-            .apply { action = ACTION_STOP }
-    }
-    private val pauseIntent by lazy(LazyThreadSafetyMode.NONE) {
-        Intent(this, ActivityTrackingForegroundService::class.java)
-            .apply { action = ACTION_PAUSE }
-    }
-    private val resumeIntent by lazy(LazyThreadSafetyMode.NONE) {
-        Intent(this, ActivityTrackingForegroundService::class.java)
-            .apply { action = ACTION_RESUME }
     }
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
+    }
+
+    private fun createPendingIntent(intent: Intent): PendingIntent {
+        return PendingIntent.getService(
+            this, 0,
+            intent,
+            PendingIntent.FLAG_CANCEL_CURRENT
+        )
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
