c9239a79ab47848e1873dfb17958020231b3df1b
Remi de Boer <remi@rsvier.nl>
2019-08-25 14:07:34 +0200

    asking location permission using Dexter

--- app/src/main/java/com/remideboer/freeactive/ui/MainActivity.kt
+++ app/src/main/java/com/remideboer/freeactive/ui/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.remideboer.freeactive.ui
 
+import android.Manifest
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import androidx.core.content.ContextCompat
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.listener.single.PermissionListener
 import com.remideboer.freeactive.R
 import com.remideboer.freeactive.services.ActivityTrackingForegroundService
 
 import kotlinx.android.synthetic.main.activity_main.*
+import com.karumi.dexter.listener.single.DialogOnDeniedPermissionListener
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,5 +30,21 @@ class MainActivity : AppCompatActivity() {
 
             ContextCompat.startForegroundService(this, Intent(this, ActivityTrackingForegroundService::class.java))
         }
+
+        askRuntimePermissions()
+    }
+
+    private fun askRuntimePermissions() {
+        val dialogPermissionListener = DialogOnDeniedPermissionListener.Builder
+            .withContext(this)
+            .withTitle("Location Permission")
+            .withMessage("Location permission is needed to keep track of your route")
+            .withButtonText(android.R.string.ok)
+            .build()
+
+        Dexter.withActivity(this)
+            .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+            .withListener(dialogPermissionListener)
+            .check()
     }
 }
